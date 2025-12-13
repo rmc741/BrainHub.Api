2e1b69face1af6d9cc294c11132c995687c760f7
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-13 03:02:21 -0300

    libs e db context

---
+++ BrainHub.Api/Data/BrainHubDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BrainHub.Api.Data
+{
+    public class BrainHubDbContext : DbContext
+    {
+        public BrainHubDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(BrainHubDbContext).Assembly);
+        }
+    }
+}

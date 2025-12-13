348aaae640f21fd0f639299cbd103ecad63ee429
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-13 03:02:05 -0300

    Artigo entity e config

---
+++ BrainHub.Api/Domain/Artigo.cs
@@ -0,0 +1,15 @@
+﻿namespace BrainHub.Api.Domain
+{
+    public class Artigo
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string Conteudo { get; set; }
+
+        public string Autor { get; set; }
+
+        public DateTime DataCriacao { get; set; } = DateTime.Now;
+    }
+}
---
+++ BrainHub.Api/Domain/EntityConfiguration/ArtigoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BrainHub.Api.Domain.EntityConfiguration
+{
+    public class ArtigoConfiguration : IEntityTypeConfiguration<Artigo>
+    {
+        public void Configure(EntityTypeBuilder<Artigo> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.Property(a => a.Titulo)
+                   .IsRequired()
+                   .HasMaxLength(200);
+
+            builder.Property(a => a.Conteudo)
+                .IsRequired();
+
+            builder.Property(a => a.Autor)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(a => a.DataCriacao);
+        }
+    }
+}

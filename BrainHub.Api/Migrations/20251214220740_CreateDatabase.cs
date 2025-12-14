af936070bcf5c3dc3e1b81c1283613303ca61249
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-14 19:24:41 -0300

    migrations

---
+++ BrainHub.Api/Migrations/20251214220740_CreateDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrainHub.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artigos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Conteudo = table.Column<string>(type: "TEXT", nullable: false),
+                    Autor = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artigos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Artigos");
+        }
+    }
+}

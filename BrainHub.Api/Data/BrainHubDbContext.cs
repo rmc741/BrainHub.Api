504a48518f166f19c9d2b2a987f9854aaf5f14fc
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-14 19:23:34 -0300

    db set artigos

--- BrainHub.Api/Data/BrainHubDbContext.cs
+++ BrainHub.Api/Data/BrainHubDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BrainHub.Api.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace BrainHub.Api.Data
 {
     public class BrainHubDbContext : DbContext
     {
         public BrainHubDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Artigo> Artigos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {

46efc32b02536550e40845d45c8c01899158c889
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-14 19:23:48 -0300

    sqlite config

--- BrainHub.Api/Program.cs
+++ BrainHub.Api/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BrainHub.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<BrainHubDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BrainHubBackContext") ?? throw new InvalidOperationException("Connection string 'BrainHubBackContext' not found.")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

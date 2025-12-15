c72d20fff04d77794aaa94b9988d69c907d0e4b7
Rafael Mendonca <rafael.decarvalho@castgroup.com.br>
2025-12-14 21:08:25 -0300

    add cors config

--- BrainHub.Api/Program.cs
+++ BrainHub.Api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BrainHub.Api.Data;
+using BrainHub.Api.Data.Interface;
+using BrainHub.Api.Data.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,12 +11,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BrainHubDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("BrainHubBackContext") ?? throw new InvalidOperationException("Connection string 'BrainHubBackContext' not found.")));
 
+builder.Services.AddScoped<IArtigoRepository, ArtigoRepository>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy
+            .AllowAnyOrigin()    // permite qualquer origem
+            .AllowAnyMethod()    // permite qualquer método (GET, POST, PUT, DELETE, etc.)
+            .AllowAnyHeader();   // permite qualquer cabeçalho
+    });
+});
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

acfef951efe012f40fd3fb6d71a8bd0c47486e21
Rafael Mendonca <rafael.medonca2@gmail.com>
2026-03-21 19:38:29 -0300

    Get artigo by id / Create artigo

--- BrainHub.Api/Controllers/ArtigoController.cs
+++ BrainHub.Api/Controllers/ArtigoController.cs
@@ -1,6 +1,5 @@
-﻿using BrainHub.Api.Application.Dtos;
+using BrainHub.Api.Application.Dtos;
 using BrainHub.Api.Data.Interface;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BrainHub.Api.Controllers
@@ -23,10 +22,44 @@ namespace BrainHub.Api.Controllers
             return Ok(result);
         }
 
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetArtigoById(int id)
+        {
+            var result = await _artigoRepository.GetArtigoById(id);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
         [HttpPost]
-        public async Task<IActionResult> CreateArtigo([FromBody] CreateArtigoDto artigoDto) {
+        public async Task<IActionResult> CreateArtigo([FromBody] CreateArtigoDto artigoDto)
+        {
+            if (string.IsNullOrWhiteSpace(artigoDto.Titulo))
+            {
+                ModelState.AddModelError(nameof(artigoDto.Titulo), "Titulo e obrigatorio.");
+            }
+
+            if (string.IsNullOrWhiteSpace(artigoDto.Conteudo))
+            {
+                ModelState.AddModelError(nameof(artigoDto.Conteudo), "Conteudo e obrigatorio.");
+            }
+
+            if (string.IsNullOrWhiteSpace(artigoDto.Autor))
+            {
+                ModelState.AddModelError(nameof(artigoDto.Autor), "Autor e obrigatorio.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
             var result = await _artigoRepository.CreateArtigo(artigoDto);
-            return Ok();
+            return CreatedAtAction(nameof(GetArtigoById), new { id = result.Id }, result);
         }
     }
 }

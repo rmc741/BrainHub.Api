0e55e91d670bf678900038aa97114e306d673225
Rafael Mendonca <rafael.medonca2@gmail.com>
2026-04-14 22:51:51 -0300

    migrations

--- BrainHub.Api/Migrations/20260415014518_AddAutorRelationshipToArtigo.Designer.cs
+++ BrainHub.Api/Migrations/20260415014518_AddAutorRelationshipToArtigo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BrainHub.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BrainHub.Api.Migrations
 {
     [DbContext(typeof(BrainHubDbContext))]
-    partial class BrainHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260415014518_AddAutorRelationshipToArtigo")]
+    partial class AddAutorRelationshipToArtigo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
---
+++ BrainHub.Api/Migrations/20260415014518_AddAutorRelationshipToArtigo.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrainHub.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAutorRelationshipToArtigo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AutorId",
+                table: "Artigos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.DropColumn(
+                name: "Autor",
+                table: "Artigos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Artigos_AutorId",
+                table: "Artigos",
+                column: "AutorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Artigos_Usuarios_AutorId",
+                table: "Artigos",
+                column: "AutorId",
+                principalTable: "Usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Artigos_Usuarios_AutorId",
+                table: "Artigos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Artigos_AutorId",
+                table: "Artigos");
+
+            migrationBuilder.DropColumn(
+                name: "AutorId",
+                table: "Artigos");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Autor",
+                table: "Artigos",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}

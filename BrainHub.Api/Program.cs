0b0dfbe9c3d5bd39e3af7395800a751d49f9ec0d
Rafael Mendonca <rafael.medonca2@gmail.com>
2026-05-27 22:09:02 -0300

    chore(swagger): enable JWT authorization

--- BrainHub.Api/Program.cs
+++ BrainHub.Api/Program.cs
@@ -6,6 +6,7 @@ using BrainHub.Api.Data.Interface;
 using BrainHub.Api.Data.Repository;
 using BrainHub.Api.Domain;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,7 +34,33 @@ builder.Services.AddControllers();
 JwtConfig.ConfigureJwt(builder.Services, builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Informe o token JWT no formato: Bearer {seu_token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
